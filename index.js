e71201f3fadcd951daf4f2e62bcde3afcb3e85c7
Kevin <691208yz@gmail.com>
2020-12-11 08:44:45 -0600

    listening setup

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-ghis
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', (req, res, next) => {
+    return res.json({
+        "text": "hello"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`)
+})
